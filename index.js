306449dbde3cb555d7c436d29aa08ea4a38c20b6
xedom <xedom08@gmail.com>
2019-05-01 18:46:28 +0200

    function to get list of rpt logs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const cfg = require('./config.json');
-const utils = require('./utils');
+const { getRptList } = require('./utils');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.get('/', (req, res) => { res.render('index.pug'); });
 
-app.get('/getRptList', (req, res) => {
-    res.json('test');
+app.get('/getRptList', async (req, res) => {
+    const list = await getRptList(cfg.logsPath);
+
+    return res.json(list);
 });
 
 app.listen(cfg.port, () => console.log(`[Server] > RPT Reader started on port: ${cfg.port}`));
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-// const cfg = require();
 
-function getRptList(path) {
+function getRptList(logspath) {
+    logspath = path.resolve(logspath);
+    const rptList = fs.readdirSync(logspath,"utf-8");
+    const filteredRptList = rptList.filter(file => path.extname(file) == '.rpt');
+    const mappedRptList = filteredRptList.map(rpt => ({ 
+        rpt: rpt,
+        name: path.basename(rpt,'.rpt'),
+        path: logspath+rpt
+    }));
 
+    return mappedRptList;
 };
+
+module.exports = { getRptList }
