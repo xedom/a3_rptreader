cb56f91cfd1594a27df43edf068aa77269220d11
xedom <xedom08@gmail.com>
2019-05-01 17:59:25 +0200

    added utils script

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const path = require('path');
+const utils = require('./utils');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.get('/', (req, res) => { res.render('index.pug'); });
+
+app.get('/getRptList', (req, res) => {
+    res.json('test');
+});
+
 app.listen(8000, () => console.log('Server Avviato'));
---
+++ utils/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+// const cfg = require();
+
+function getRptList(path) {
+
+};
